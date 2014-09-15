8288e6243cdf2eaf2f9d436ced2adaf5a4e98b76
三薗義隆 <misono@eros.sos.info.hiroshima-cu.ac.jp>
2014-09-15 15:48:30 +0900

    test server add

---
+++ Fusang/testshare_server.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+require 'socket'
+require 'mysql'
+require 'json'
+
+server = TCPServer.new(23627)
+db = Mysql.connect('localhost','ricoh_client','akioohba','ricoh')
+stmt = db.prepare('INSERT INTO test_table (subject,sendername) VALUES(?,?)')
+
+loop do
+	socket = server.accept
+	p "Connection establised!"
+	#p socket.peeraddr
+	while buffer = socket.gets
+		#p buffer
+		receivedData = JSON.parse(buffer)
+	end
+	p receivedData
+	stmt.execute receivedData["subject"],receivedData["sendername"]
+	p "Terminating connection."
+	socket.close
+end
+
+#should not come here
+server.close
